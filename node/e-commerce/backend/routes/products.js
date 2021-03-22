e55fec16dca45ab45322e41844edf8c76106ffbf
oimoralest <oimoralest@gmail.com>
2021-03-22 12:48:25 -0500

    Added the routes. for the API

---
+++ node/e-commerce/backend/routes/products.js
@@ -0,0 +1,37 @@
+// Importing
+const {Product} = require('../models/product');
+const express = require('express');
+const router = express.Router();
+
+// GET
+router.get('/', async (req, res) => {
+	const productList = await Product.find();
+	if (!productList) {
+		res.status(500).json({
+			success: false,
+		});
+	}
+	res.send(productList);
+});
+
+// POST
+router.post('/', (req, res) => {
+	const newProduct = new Product({
+		name: req.body.name,
+		image: req.body.image,
+		countInStock: req.body.countInStock,
+	});
+	newProduct
+		.save()
+		.then((createdProduct) => {
+			res.status(201).json(createdProduct);
+		})
+		.catch((err) => {
+			res.status(500).json({
+				error: err,
+				success: false,
+			});
+		});
+});
+
+module.exports = router;
