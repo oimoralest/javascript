8b762bd4b261b3580c9376e00b0778481a36168f
oimoralest <oimoralest@gmail.com>
2021-03-24 20:06:58 -0500

    Configured product route and schema

--- node/e-commerce/backend/models/product.js
+++ node/e-commerce/backend/models/product.js
@@ -3,13 +3,64 @@ const mongoose = require('mongoose');
 
 // Schema
 const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
-        required: true
-    }
-})
+	name: {
+		type: String,
+		required: true,
+	},
+	description: {
+		type: String,
+		required: true,
+	},
+	richDescription: {
+		type: String,
+		default: '',
+	},
+	image: {
+		type: String,
+		default: '',
+	},
+	images: [
+		{
+			type: String,
+		},
+	],
+	brand: {
+		type: String,
+		default: '',
+	},
+	price: {
+		type: Number,
+		default: 0,
+		required: true,
+	},
+	category: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: 'Category',
+		required: true,
+	},
+	countInStock: {
+		type: Number,
+		required: true,
+		min: 0,
+		max: 255,
+	},
+	rating: {
+		type: Number,
+		default: 0,
+	},
+	numReviews: {
+		type: Number,
+		default: 0,
+	},
+	isFeatured: {
+		type: Boolean,
+		default: false,
+	},
+	dateCreated: {
+		type: Date,
+		default: Date.now,
+	},
+});
 
 // Exporting
 exports.Product = mongoose.model('Product', productSchema);
--- node/e-commerce/backend/routes/products.js
+++ node/e-commerce/backend/routes/products.js
@@ -1,37 +1,105 @@
 // Importing
 const {Product} = require('../models/product');
 const express = require('express');
+const {Category} = require('../models/category');
 const router = express.Router();
 
 // GET
+// All products
 router.get('/', async (req, res) => {
-	const productList = await Product.find();
+	const productList = await Product.find().populate('category');
 	if (!productList) {
-		res.status(500).json({
-			success: false,
-		});
+		return res.status(500).json({success: false});
 	}
-	res.send(productList);
+	res.status(200).send(productList);
+});
+
+// Product by id
+router.get('/:id', async (req, res) => {
+	const product = await Product.findById(req.params.id).populate('category');
+	if (!product) {
+		return res.status('500').json({message: 'Product not found!'});
+	}
+	res.status(200).send(product);
 });
 
 // POST
-router.post('/', (req, res) => {
-	const newProduct = new Product({
+router.post('/', async (req, res) => {
+	const category = await Category.findOne({name: req.body.category});
+	if (!category) {
+		return res.status(500).send('Indalid category');
+	}
+	let product = new Product({
 		name: req.body.name,
+		description: req.body.description,
+		richdescription: req.body.richdescription,
 		image: req.body.image,
+		images: req.body.images,
+		brand: req.body.brand,
+		price: req.body.price,
+		category: category._id,
 		countInStock: req.body.countInStock,
+		rating: req.body.rating,
+		numReviews: req.body.numReviews,
+		isFeatured: req.body.isFeatured,
 	});
-	newProduct
-		.save()
-		.then((createdProduct) => {
-			res.status(201).json(createdProduct);
-		})
-		.catch((err) => {
-			res.status(500).json({
-				error: err,
-				success: false,
+	product = await product.save();
+	if (!product) {
+		return res.status(500).send('The product could not be created!');
+	}
+	res.send(product);
+});
+
+// PUT
+router.put('/:category_id', async (req, res) => {
+	const category = await Category.findById(req.params.category_id);
+	if (!category) {
+		return res.status(500).send('Indalid category');
+	}
+	const product = await Product.findByIdAndUpdate(
+		req.params.id,
+		{
+			name: req.body.name,
+			description: req.body.description,
+			richdescription: req.body.richdescription,
+			image: req.body.image,
+			images: req.body.images,
+			brand: req.body.brand,
+			price: req.body.price,
+			category: req.params.category_id,
+			countInStock: req.body.countInStock,
+			rating: req.body.rating,
+			numReviews: req.body.numReviews,
+			isFeatured: req.body.isFeatured,
+		},
+		{
+			new: true,
+		},
+	);
+	if (!product) {
+		return res.status(500).send('The product could not be updated!');
+	}
+	res.send(product);
+});
+
+// DELETE
+router.delete('/:id', async (req, res) => {
+	try {
+		const product= await Product.findByIdAndRemove(req.params.id);
+		if (product) {
+			res.status(200).json({
+				success: true,
+				message: 'Product successfully deleted',
 			});
-		});
+		} else {
+			res.status(404).json({
+				success: false,
+				message: 'Product not found!',
+			});
+		}
+	} catch (error) {
+		res.status(400).send(error);
+	}
 });
 
 module.exports = router;
