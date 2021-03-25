b0696aba3a3b0c657a0630734fb5adf7ad079a66
oimoralest <oimoralest@gmail.com>
2021-03-24 19:39:54 -0500

    Configured category route and schema

--- node/e-commerce/backend/models/category.js
+++ node/e-commerce/backend/models/category.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose');
 
 const categorySchema = mongoose.Schema({
-
-})
+	name: {
+		type: String,
+		required: true,
+	},
+	color: {
+		type: String,
+	},
+	icon: {
+		type: String,
+	},
+});
 
 exports.Category = mongoose.model('Category', categorySchema);
