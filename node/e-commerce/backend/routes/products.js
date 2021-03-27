aea95e41578095aaf19b69d002c0337dd24ed889
oimoralest <oimoralest@gmail.com>
2021-03-27 10:13:27 -0500

    Implemented image upload

--- node/e-commerce/backend/routes/products.js
+++ node/e-commerce/backend/routes/products.js
@@ -3,6 +3,32 @@ const {Product} = require('../models/product');
 const express = require('express');
 const {Category} = require('../models/category');
 const router = express.Router();
+const multer = require('multer');
+
+const FILE_TYPE_MAP = {
+	'image/png': 'png',
+	'image/jpeg': 'jpeg',
+	'image/jpg': 'jpg',
+};
+
+// Images storage
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		const isValid = FILE_TYPE_MAP[file.mimetype];
+		let UploadError = new Error('Invalid image type');
+		if (isValid) {
+			UploadError = null;
+		}
+		cb(UploadError, 'public/uploads');
+	},
+	filename: function (req, file, cb) {
+		const fileName = file.originalname.replace(' ', '-');
+		const extension = FILE_TYPE_MAP[file.mimetype];
+		cb(null, `${fileName}-${Date.now()}.${extension}`);
+	},
+});
+
+const uploadOptions = multer({storage: storage});
 
 // GET
 // All products
@@ -60,20 +86,25 @@ router.get('/', async (req, res) => {
 });
 
 // POST
-router.post('/', async (req, res) => {
-	const category = await Category.findOne({name: req.body.category});
+router.post('/', uploadOptions.single('image'), async (req, res) => {
+	const category = await Category.findById(req.body.category);
 	if (!category) {
-		return res.status(500).send('Indalid category');
+		return res.status(400).send('Indalid category');
 	}
+	const file = req.file;
+	if (!file) {
+		return res.status(400).send('No file uploaded!');
+	}
+	const fileName = req.file.filename;
+	const basePath = `${req.protocol}://${req.get('host')}/public/upload/`;
 	let product = new Product({
 		name: req.body.name,
 		description: req.body.description,
 		richdescription: req.body.richdescription,
-		image: req.body.image,
-		images: req.body.images,
+		image: `${basePath}${fileName}`,
 		brand: req.body.brand,
 		price: req.body.price,
-		category: category.id,
+		category: category._id,
 		countInStock: req.body.countInStock,
 		rating: req.body.rating,
 		numReviews: req.body.numReviews,
@@ -88,7 +119,7 @@ router.post('/', async (req, res) => {
 
 // PUT
 router.put('/:id', async (req, res) => {
-	const category = await Category.findOne({name: req.body.category});
+	const category = await Category.findById(req.body.category);
 	if (!category) {
 		return res.status(500).send('Indalid category');
 	}
@@ -102,7 +133,7 @@ router.put('/:id', async (req, res) => {
 			images: req.body.images,
 			brand: req.body.brand,
 			price: req.body.price,
-			category: category.id,
+			category: category._id,
 			countInStock: req.body.countInStock,
 			rating: req.body.rating,
 			numReviews: req.body.numReviews,
@@ -117,6 +148,35 @@ router.put('/:id', async (req, res) => {
 	}
 	res.send(product);
 });
+
+router.put(
+	'/galley-images/:id',
+	uploadOptions.array('images', 10),
+	async (req, res) => {
+		const files = req.files;
+		if (!files) {
+			return res.status(400).send('No uploaded files')
+		}
+		const basePath = `${req.protocol}://${req.get('host')}/public/upload/`;
+		let imagesPath = []
+		for (let file of files) {
+			imagesPath.push(`${basePath}${file.fileName}`)
+		}
+		const product = await Product.findByIdAndUpdate(
+			req.params.id,
+			{
+				images: imagesPath
+			},
+			{
+				new: true,
+			},
+		);
+		if (!product) {
+			return res.status(500).send('The product could not be updated!');
+		}
+		res.send(product);
+	},
+);
 
 // DELETE
 router.delete('/:id', async (req, res) => {
