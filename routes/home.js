cdfc528a9337f2b8d12d2ddf8314b3960c9ed391
oimoralest <oimoralest@gmail.com>
2021-04-22 20:34:11 -0500

    Deleted res.set and implemented res.locals

--- routes/home.js
+++ routes/home.js
@@ -6,18 +6,17 @@ const homeRouter = express.Router();
 const apiKey = process.env.API_KEY;
 const apiBaseUrl = process.env.API_BASE_URL;
 const nowPlayingUrl = `${apiBaseUrl}/movie/now_playing?api_key=${apiKey}`;
-const imageBaseUrl = process.env.IMG_BASE_URL;
 
 homeRouter.get('/', async (req, res, next) => {
     try {
         const resp = await axios.get(nowPlayingUrl)
-        if (resp.status != 200) {
+        if (resp.status !== 200) {
             throw 'Error'
         }
-        res.set('Content-Security-Policy', "img-src 'self' https://image.tmdb.org")
         res.render('index',{
+            title: 'Now in theaters',
             movies: resp.data,
-            imageBaseUrl: imageBaseUrl
+            imageBaseUrl: res.locals.imageBaseUrl
         })
     } catch {
         res.render('notFound')
