3f8f187dee84bf7527b6cb646eb16bae40b4ce73
oimoralest <oimoralest@gmail.com>
2021-04-24 08:08:48 -0500

    Applied Prettier linter

--- middlewares/contentSecurityPolicy.js
+++ middlewares/contentSecurityPolicy.js
@@ -1,4 +1,4 @@
 export const imagesPolicy = function (req, res, next) {
-    res.set('Content-Security-Policy', "img-src 'self' https://image.tmdb.org")
-    next();
-}
+	res.set('Content-Security-Policy', "img-src 'self' https://image.tmdb.org");
+	next();
+};
