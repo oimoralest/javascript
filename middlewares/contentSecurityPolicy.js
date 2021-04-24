c30454874ae3c76767c5888bada2f8846928c8b9
oimoralest <oimoralest@gmail.com>
2021-04-24 09:57:20 -0500

    Restored repo

--- middlewares/contentSecurityPolicy.js
+++
@@ -1,4 +0,0 @@
-export const imagesPolicy = function (req, res, next) {
-	res.set('Content-Security-Policy', "img-src 'self' https://image.tmdb.org");
-	next();
-};
--- middlewares/imageBaseUrl.js
+++
@@ -1,6 +0,0 @@
-const imageBaseUrl = process.env.IMG_BASE_URL;
-
-export const imageBaseURL = function (req, res, next) {
-	res.locals.imageBaseUrl = imageBaseUrl;
-	next();
-};
